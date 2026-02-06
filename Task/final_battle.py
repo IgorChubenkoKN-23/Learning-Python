4edbcc1ab6be3195490448053d1456c581fad2ca
IgorChubenkoKN-23 <igorchubenkogit@gmail.com>
2026-02-06 14:05:17 +0200

    Function Key 6Function Key 6Function Key 6Function Key 6Function Key 6Function Key 6Finl loops dictionaries

---
+++ Task/final_battle.py
@@ -0,0 +1,12 @@
+player = {"hp": 100, "damage": 20}
+monster = {"name": "Orc", "hp": 60, "damage": 15}
+
+while player["hp"] > 0 and monster["hp"]>0:
+  print(player["hp"], monster["hp"])
+  monster["hp"] -= player["damage"]
+  if monster["hp"] > 0:
+    player["hp"] -= monster["damage"]
+    
+else:
+  print(player["hp"], monster["hp"])
+
